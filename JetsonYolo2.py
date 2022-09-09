1e7518603b9baa68cc8b0afb97ffc8abd5ea8159
pandakov <vpandakov@gmail.com>
2022-09-09 18:06:10 +0300

    Main detection script for rover

--- JetsonYolo2.py
+++ JetsonYolo2.py
@@ -1,38 +1,45 @@
-import cv2 
-import numpy as np 
-from elements.yolo import OBJ_DETECTION 
+import cv2
+import numpy as np
+from elements.yolo import OBJ_DETECTION
 
 Object_classes = ['cig_butt']
-Object_colors = list(np.random.rand(80,3)*255) 
+Object_colors = list(np.random.rand(80, 3) * 255)
 Object_detector = OBJ_DETECTION('weights/best.pt', Object_classes)
 
-cap = cv2.VideoCapture(0) 
-if cap.isOpened(): 
-        window_handle = cv2.namedWindow("USB Camera", cv2.WINDOW_AUTOSIZE) 
-        # Window 
-        while cv2.getWindowProperty("USB Camera", 0) >= 0: 
-                ret, frame = cap.read() 
-                if ret: 
-                        # detection process 
-                        objs = Object_detector.detect(frame) 
+# get perspective transformation (calibrate camera)
+input_pts = np.float32([[0, 0], [800, 800], [0, 800], [800, 0]])
+output_pts = np.float32([[0, 0], [800, 800], [0, 800], [800, 0]])
+warp_matrix = cv2.getPerspectiveTransform(input_pts, output_pts)
 
-                        # plotting 
-                        for obj in objs: 
-                                # print(obj) 
-                                label = obj['label'] 
-                                score = obj['score'] 
-                                [(xmin,ymin),(xmax,ymax)] = obj['bbox']
-                                center = ((xmax - xmin) / 2, (ymax - ymin) / 2)
-                                color = Object_colors[Object_classes.index(label)] 
-                                frame = cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), color, 2)
-                                frame = cv2.putText(frame, f'{label} ({str(score)}) {center} ', (xmin,ymin),
-                 cv2.FONT_HERSHEY_SIMPLEX , 0.75, color, 1, cv2.LINE_AA) 
+cap = cv2.VideoCapture(0)
+if cap.isOpened():
+    window_handle = cv2.namedWindow("USB Camera", cv2.WINDOW_AUTOSIZE)
+    # Window
+    while cv2.getWindowProperty("USB Camera", 0) >= 0:
+        ret, frame = cap.read()
+        if ret:
+            # detection process
+            objs = Object_detector.detect(frame)
 
-                cv2.imshow("USB Camera", frame) 
-                keyCode = cv2.waitKey(30) 
-                if keyCode == ord('q'): 
-                        break 
-        cap.release() 
-        cv2.destroyAllWindows() 
-else: 
-        print("Unable to open camera") 
+            # plotting
+            for obj in objs:
+                # print(obj)
+                label = obj['label']
+                score = obj['score']
+                [(xmin, ymin), (xmax, ymax)] = obj['bbox']
+                center = ((xmax - xmin) / 2, (ymax - ymin) / 2)
+                color = Object_colors[Object_classes.index(label)]
+                frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
+                frame = cv2.putText(frame, f'{label} ({str(score)}) {center} ', (xmin, ymin),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 1, cv2.LINE_AA)
+                warped_center = cv2.warpPerspective(center, warp_matrix, (800, 600),
+                                                    flags=cv2.INTER_LINEAR)
+
+        cv2.imshow("USB Camera", frame)
+        keyCode = cv2.waitKey(30)
+        if keyCode == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+else:
+    print("Unable to open camera")
